e1dde6d5d53dd8743e81aa9f3d8074f11e03c1f8
Jaeyoung Lee <jylee0121@gmail.com>
2017-04-16 08:05:02 +0000

    generator bugfix
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- model.py
+++ model.py
@@ -2,94 +2,86 @@ import csv
 import cv2
 import numpy as np
 import os
+import sys
 import random
+import matplotlib.pyplot as plt
 
 import sklearn
 from sklearn.model_selection import train_test_split
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Reshape, Dense, Dropout, Flatten, Input
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Lambda, Cropping2D
 from keras.models import load_model
 from keras import backend as K
 
-DATA_DIR = './data/'
-IMG_DIR = DATA_DIR + '/IMG/'
+DATA_DIR = './DATA/'
+IMG_SUBDIR = '/IMG/'
 
 def load_samples():
   samples = []
-  with open(os.path.join(DATA_DIR, './driving_log.csv')) as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-      samples.append(line)
+  for dirpath in os.listdir(DATA_DIR):
+    print(dirpath)
+    try:
+      with open(os.path.join(DATA_DIR + dirpath, './driving_log.csv')) as csvfile:
+        reader = csv.reader(csvfile)
+        # skip fitst line
+        for i, line in enumerate(reader):
+          if i > 0:
+            samples.append((DATA_DIR + dirpath, line))
+    except Exception as e:
+      print(e)
   return samples
 
+def load_img(dirpath, filepath):
+  filename = filepath.split('/')[-1]
+  path = os.path.join(dirpath + IMG_SUBDIR, filename)
+  img = cv2.imread(path)
+  return img
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=32, augment_factor=6):
+  correction = 0.2 # this is a parameter to tune
   num_samples = len(samples)
+
   while 1: # Loop forever so the generator never terminates
     random.shuffle(samples)
-    for offset in range(0, num_samples, batch_size):
-      batch_samples = samples[offset:offset+batch_size]
+    for offset in range(0, num_samples, batch_size//augment_factor):
+      batch_samples = samples[offset:offset+batch_size//augment_factor]
 
       images = []
       angles = []
       for batch_sample in batch_samples:
-        name = IMG_DIR+batch_sample[0].split('/')[-1]
-        center_image = cv2.imread(name)
-        center_angle = float(batch_sample[3])
-        images.append(center_image)
-        angles.append(center_angle)
+        dirpath, sample = batch_sample
+
+        center_image = load_img(dirpath, sample[0])
+        left_image   = load_img(dirpath, sample[1])
+        right_image  = load_img(dirpath, sample[2])
+        center_angle = float(sample[3])
+        left_angle  = center_angle + correction
+        right_angle = center_angle - correction
+
+        images.extend([center_image, left_image, right_image])
+        angles.extend([center_angle, left_angle, right_angle])
+
+        # flip
+        images.extend([cv2.flip(center_image, 1), cv2.flip(left_image, 1), cv2.flip(right_image, 1)])
+        angles.extend([-center_angle, -left_angle, -right_angle])
 
       # trim image to only see section with road
       X_train = np.array(images)
       y_train = np.array(angles)
-      yield sklearn.utils.shuffle(X_train, y_train)
-
-#def load_img(src_path):
-#  filename = src_path.split('/')[-1]
-#  current_path = './data/IMG/' + filename
-#  img = cv2.imread(current_path)
-#  return img
-#
-#def load_data():
-#  lines = []
-#  with open('./data/driving_log.csv') as csvfile:
-#    readlines = csv.reader(csvfile)
-#    for line in readlines:
-#      lines.append(line)
-#
-#  images = []
-#  steering_angles = []
-#  for line in lines[1:100]:
-#
-#    img_center = load_img(line[0])
-#    img_left   = load_img(line[1])
-#    img_right  = load_img(line[2])
-#
-#    images.extend([img_center, img_left, img_right])
-#
-#    # create adjusted steering measurements for the side camera images
-#    steering_center = float(line[3])
-#    correction = 0.2 # this is a parameter to tune
-#    steering_left = steering_center + correction
-#    steering_right = steering_center - correction
-#    steering_angles.extend([steering_center, steering_left, steering_right])
-#
-#    # augment data by flipping
-#    images.extend([cv2.flip(img_center, 1), cv2.flip(img_left, 1), cv2.flip(img_right, 1)])
-#    steering_angles.extend([-steering_center, -steering_left, -steering_right])
-#
-#  return images, steering_angles
+      #yield sklearn.utils.shuffle(X_train, y_train)
+      yield (X_train, y_train)
 
 def main():
   model_file = 'model.h5'
-  #batch_size = 32
-  batch_size = 1
-  epochs = 12
+  batch_size = 192
+  augment_factor=6
+  epochs = 2
 
+  # prepare data
   samples = load_samples()
   train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
@@ -97,20 +89,17 @@ def main():
     print('channels_first')
   else:
     print(K.image_data_format())
-  # prepare data
-#  _x, _y = load_data()
-#  size = len(_x)
-#  test_size = int(size*0.1)
-#  x_train, x_test = np.array(_x[test_size:]), np.array(_x[:test_size])
-#  y_train, y_test = np.array(_y[test_size:]), np.array(_y[:test_size])
 
-# compile and train the model using the generator function
+  # set generator function
   train_generator = generator(train_samples, batch_size=batch_size)
   validation_generator = generator(validation_samples, batch_size=batch_size)
 
+  print("num_samples:{}".format(len(samples)))
+  print("num_batches:{}".format(len(samples)//batch_size))
 
   # setup model
   input_shape =(160, 320, 3)
+  reshape_shape =(160, 320, 3)
   print('Input Shape: {}'.format(input_shape))
 
   if os.path.exists(model_file):
@@ -119,16 +108,13 @@ def main():
   else:
     print(">> model initialize")
     model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
-    model.add(Cropping2D(cropping=((70, 25), (0,0))))
-#    model.add(Conv2D(24, kernel_size=(5, 5), sub_sample=(2, 2), activation='relu'))
-#    model.add(Conv2D(36, kernel_size=(5, 5), sub_sample=(2, 2), activation='relu'))
-#    model.add(Conv2D(48, kernel_size=(5, 5), sub_sample=(2, 2), activation='relu'))
-#    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Cropping2D(cropping=((70, 25), (0,0)), input_shape=input_shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-#   model.add(MaxPooling2D(pool_size=(2, 2)))
-#   #model.add(Dropout(0.25))
-#   model.add(Dense(128, activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
     model.add(Dropout(0.5))
@@ -141,22 +127,26 @@ def main():
                 optimizer='adam',
                 metrics=['accuracy'])
 
-#  model.fit(x_train, y_train,
-#            batch_size=batch_size,
-#            epochs=epochs,
-#            verbose=1,
-#            shuffle=True,
-#            validation_split=0.2)
-  model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)//batch_size),
+  history_object = model.fit_generator(train_generator, steps_per_epoch=(augment_factor*len(train_samples)//batch_size),
       validation_data=validation_generator, validation_steps=len(validation_samples)//batch_size,
       epochs=epochs)
 
-  score = model.evaluate(x_test, y_test, verbose=1)
-  print(model.metrics_names)
-  print('Test loss:', score[0])
-  print('Test accuracy:', score[1])
-
+  # save model
   model.save(model_file)
+  print('saving model done.')
+
+  ### print the keys contained in the history object
+  print(history_object.history.keys())
+  ### plot the training and validation loss for each epoch
+  plt.plot(history_object.history['loss'])
+  plt.plot(history_object.history['val_loss'])
+  plt.title('model mean squared error loss')
+  plt.ylabel('mean squared error loss')
+  plt.xlabel('epoch')
+  plt.legend(['training set', 'validation set'], loc='upper right')
+  plt.savefig('visualization.png')
+
+  print('saving loss visualization done.')
 
 if __name__ == '__main__':
   main()
